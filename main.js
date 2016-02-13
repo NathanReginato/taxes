9bcb90e01bc3a852504abe902477730b4944e325
Nathan Reginato <nathan.reginato@gmail.com>
2016-02-13 12:30:57 -0700

    Found a strange error in JS where large decimal is returned. Created a new test suite to test error

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 module.exports = {
   calcTax: function (amount) {
     var taxAmount = 0;
-    taxAmount = amount * 0.1;
+    taxAmount = +(amount * 0.03).toFixed(1);
     return taxAmount;
+  },
+  calcTaxError: function (amount) {
+    var taxAmountError = 0;
+    taxAmountError = amount * 0.03;
+    return taxAmountError;
   }
 }
--- test/test.js
+++ test/test.js
@@ -6,13 +6,19 @@ describe('Tax Calculator', function(){
       expect(code.calcTax(0)).to.equal(0);
     });
 
-  it('should tax 10% on the first $10', function() {
-    expect(code.calcTax(1)).to.equal(0.1);
-    expect(code.calcTax(10)).to.equal(1);
+  it('should tax 3% on total amount', function() {
+    expect(code.calcTax(10)).to.equal(0.3);
+    expect(code.calcTax(30)).to.equal(0.9);
   });
   //
   // it('should tax 7% on the second $10', function(){
   //   expect(code.calcTax(15)).to.equal(1.35);
   // });
 
+});
+
+describe('Tax Calculator Error', function(){
+  it('should return a large decimal number', function() {
+    expect(code.calcTaxError(30)).to.equal(0.8999999999999999);
+  });
 });
