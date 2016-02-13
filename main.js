b8491e1d9c4e97aed8e41d85d84fa6e6e42e55fa
Nathan Reginato <nathan.reginato@gmail.com>
2016-02-13 11:52:20 -0700

    Added set of tests to test on first . All tests pass as expected

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 module.exports = {
   calcTax: function (amount) {
-    var taxedAmount = 0;
-    return taxedAmount;
+    var taxAmount = 0;
+    taxAmount = amount * 0.1;
+    return taxAmount;
   }
 }
--- test/test.js
+++ test/test.js
@@ -3,13 +3,13 @@ var expect = require('chai').expect;
 
 describe('Tax Calculator', function(){
     it('should return taxedAmount zero', function() {
-      expect(code.calcTax()).to.equal(0);
+      expect(code.calcTax(0)).to.equal(0);
     });
 
-  // it('should tax 10% on the first $10', function() {
-  //   expect(code.calcTax(1)).to.equal(0.1);
-  //   expect(code.calcTax(10)).to.equal(1);
-  // });
+  it('should tax 10% on the first $10', function() {
+    expect(code.calcTax(1)).to.equal(0.1);
+    expect(code.calcTax(10)).to.equal(1);
+  });
   //
   // it('should tax 7% on the second $10', function(){
   //   expect(code.calcTax(15)).to.equal(1.35);
