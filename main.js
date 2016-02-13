2847b8ffca752469243f13b9bd8ce7479d1fbb7e
Nathan Reginato <nathan.reginato@gmail.com>
2016-02-13 13:27:03 -0700

    Everything after  get taxed by 7%

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 module.exports = {
   calcTax: function (amount) {
     var taxAmount = 0;
-    taxAmount = Math.round((amount  * 0.1)*100)/100;
+    if ( amount > 10 ) {
+      taxAmount = 10 * 0.1;
+      taxAmount += (amount - 10) * 0.07;
+      return taxAmount;
+    }
+    taxAmount = amount * 0.1;
     return taxAmount;
   }
 }
--- test/test.js
+++ test/test.js
@@ -5,9 +5,13 @@ describe('Tax Calculator', function(){
   it('should return taxedAmount zero', function() {
     expect(code.calcTax(0)).to.equal(0);
   });
-  
+
   it('should tax 10% on the first $10', function() {
     expect(code.calcTax(1)).to.equal(0.1);
     expect(code.calcTax(10)).to.equal(1);
+  });
+
+  it('should tax 7% on the second $10', function(){
+    expect(code.calcTax(15)).to.equal(1.35);
   });
 });
