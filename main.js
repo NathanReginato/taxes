c85187b36f4c194a7489950d1d6886262f09e6c7
Nathan Reginato <nathan.reginato@gmail.com>
2016-02-13 13:09:09 -0700

    Ive taken the wrong approach. Im starting over. Variables are declared first test passes as expected

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 module.exports = {
   calcTax: function (amount) {
     var taxAmount = 0;
-    taxAmount = +(amount * 0.03).toFixed(1);
     return taxAmount;
-  },
-  calcTaxError: function (amount) {
-    var taxAmountError = 0;
-    taxAmountError = amount * 0.03;
-    return taxAmountError;
   }
 }
--- test/test.js
+++ test/test.js
@@ -2,23 +2,7 @@ var code = require('../main');
 var expect = require('chai').expect;
 
 describe('Tax Calculator', function(){
-    it('should return taxedAmount zero', function() {
-      expect(code.calcTax(0)).to.equal(0);
-    });
-
-  it('should tax 3% on total amount', function() {
-    expect(code.calcTax(10)).to.equal(0.3);
-    expect(code.calcTax(30)).to.equal(0.9);
-  });
-  
-  it('should tax 7% on the second $10', function(){
-    expect(code.calcTax(15)).to.equal(1.35);
-  });
-
-});
-
-describe('Tax Calculator Error', function(){
-  it('should return a large decimal number', function() {
-    expect(code.calcTaxError(30)).to.equal(0.8999999999999999);
+  it('should return taxedAmount zero', function() {
+    expect(code.calcTax(0)).to.equal(0);
   });
 });
